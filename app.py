9c24345c3b3e7bbd615cd37061baaa8ac814bc25
spravinraj78-cpu <spravinraj78@gmail.com>
2026-09-06 13:52:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
+import os
 from flask import Flask, render_template
 
-app = Flask(__name__)
+# Absolute path set panroam so Vercel templates-a correct-a edukkum
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
 @app.route('/')
 def home():
-    profile_data = {
-        "name": "Pravin Raj",
-        "bio": "Content Creator | Tech Enthusiast",
-        "insta_url": "https://instagram.com/pravin_official14",
-        "yt_url": "https://youtube.com/@UNGA_YOUTUBE_CHANNEL"
-    }
-    return render_template('index.html', data=profile_data)
+    return render_template('index.html') # Unga html file name dhaan index.html
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(debug=True)
